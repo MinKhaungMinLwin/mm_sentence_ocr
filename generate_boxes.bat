2a19692c6dd10d6b16e53f56f99b6f4670110f55
KoPyae455 <kopyaegtr455@gmail.com>
2025-04-02 22:06:20 +0700

    window bat file updated

--- generate_boxes.bat
+++ generate_boxes.bat
@@ -1,7 +1,32 @@
 @echo off
 chcp 65001 >nul
-for %%f in (uncleaned\pic\*.png) do (
-    echo Processing: %%~nxf
-    tesseract "%%f" "uncleaned\gt\%%~nf" --psm 6 -l mya makebox
+setlocal enabledelayedexpansion
+
+:: Set working directory to script's directory
+cd /d "%~dp0"
+
+:: Check and create 'gt' folder if not exists
+if not exist "uncleaned\gt" mkdir "uncleaned\gt"
+
+:: Rename files safely
+for %%f in ("uncleaned\pic\*.png") do (
+    set "name=%%~nxf"
+    set "newname=!name: =_!"
+    set "newname=!newname:(=_!"
+    set "newname=!newname:)=_!"
+    set "newname=!newname:၊=_!"
+    set "newname=!newname:“=_!"
+    set "newname=!newname:”=_!"
+    set "newname=!newname:‘=_!"
+    set "newname=!newname:’=_!"
+    ren "uncleaned\pic\%%~nxf" "!newname!"
 )
+
+:: Run Tesseract OCR correctly after renaming
+for %%f in ("uncleaned\pic\*.png") do (
+    echo Processing: %%~nxf
+    tesseract "uncleaned\pic\%%~nxf" "uncleaned\gt\%%~nf" --psm 6 -l mya makebox
+)
+
+echo Done!
 pause
